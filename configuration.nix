28e07f2b673186bcd7db3de07065997b4330ca98
LinSoap <linsoap1024@outlook.com>
2025-07-05 00:25:14 +0800

    切换版本为25.05，unstable折腾不起...

--- configuration.nix
+++ configuration.nix
@@ -73,7 +73,7 @@
     LC_TIME = "zh_CN.UTF-8";
   };
   i18n.inputMethod = {
-    enabled = "fcitx5";
+    enable = true;
     type = "fcitx5";
     fcitx5.addons = with pkgs; [
       fcitx5-rime
@@ -248,6 +248,6 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  system.stateVersion = "25.05"; # Did you read the comment?
 
 }
