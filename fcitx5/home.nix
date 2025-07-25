18118412685c141a634076ccaec7f3412a090798
LinSoap <linsoap1024@outlook.com>
2025-07-25 11:19:36 +0800

    feat: 配置 fcitx5 使用自定义目录路径

--- fcitx5/home.nix
+++ fcitx5/home.nix
@@ -1,5 +1,14 @@
 { config, pkgs, ... }:
 
 {
-  home.file."fcitx5".source = ./fcitx5;
+  # 直接将当前目录下的 fcitx5 配置链接到默认位置
+  home.file.".local/share/fcitx5" = {
+    source = ./fcitx5;
+    recursive = true;
+  };
+  
+  # 也可以设置环境变量指向自定义位置（可选）
+  # home.sessionVariables = {
+  #   FCITX_DATA_HOME = "${config.home.homeDirectory}/my-nix/fcitx5";
+  # };
 }
