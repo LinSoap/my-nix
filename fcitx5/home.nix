4b10cfd31cbe9a71f12f817dbe8e91395fd97417
LinSoap <linsoap1024@outlook.com>
2025-07-25 13:49:27 +0800

    更新雙拼

--- fcitx5/home.nix
+++ fcitx5/home.nix
@@ -2,13 +2,14 @@
 
 {
   # 直接将当前目录下的 fcitx5 配置链接到默认位置
-  home.file.".local/share/fcitx5" = {
-    source = ./fcitx5;
+  home.file.".local/share/fcitx5/rime" = {
+    source = ./fcitx5/rime;
     recursive = true;
+    force = true; # 强制覆盖现有文件
   };
-  
-  # 也可以设置环境变量指向自定义位置（可选）
-  # home.sessionVariables = {
-  #   FCITX_DATA_HOME = "${config.home.homeDirectory}/my-nix/fcitx5";
-  # };
+  home.file.".local/share/fcitx5/themes" = {
+    source = ./fcitx5/themes;
+    recursive = true;
+    force = true; # 强制覆盖现有文件
+  };
 }
